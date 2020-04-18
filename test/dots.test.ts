df47e32d7667ea72f8ec44a834c21439db6a73ef
Nikolai Kiselev <10960944+nikkiselev@users.noreply.github.com>
2020-04-18 15:02:40 +0400

    Update interface

--- test/dots.test.ts
+++ test/dots.test.ts
@@ -1,18 +1,18 @@
 describe('DOTS', () => {
   it('should calculate DOTS points for males', async () => {
-    expect(dots(80.5, 700, 'male')).toBe(480.92)
+    expect(dots(80.5, 700, 'm')).toBe(480.92)
   })
 
   it('should calculate DOTS points for females', async () => {
-    expect(dots(62.15, 400, 'female')).toBe(433.8)
+    expect(dots(62.15, 400, 'f')).toBe(433.8)
   })
 
   it('should calculate DOTS points in kg by default', async () => {
-    expect(dots(62.15, 400, 'female')).toBe(433.8)
+    expect(dots(62.15, 400, 'f')).toBe(433.8)
   })
 
   it('should calculate DOTS points in pounds if specified', async () => {
-    expect(dots(137, 882, 'female', 'lb')).toBe(433.8)
+    expect(dots(137, 882, 'f', 'lb')).toBe(433.8)
     // or 516.37
   })
 })
