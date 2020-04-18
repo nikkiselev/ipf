e3b5c48d7c67557411b4d64d847f0000295ee0d1
Nikolai Kiselev <10960944+nikkiselev@users.noreply.github.com>
2020-04-18 16:59:27 +0400

    Refactor

--- src/dots.ts
+++ src/dots.ts
@@ -1,21 +1,11 @@
-import { Gender, DOTS } from './types'
+import { DOTS } from './types'
 import kg2lbs from './libs/kg2lbs'
 import format from './libs/format'
+import coefficient from './libs/coefficient'
 
 const params = {
   m: [-1.093e-6, 7.391293e-4, -0.1918759221, 24.0900756, -307.75076],
   f: [-1.0706e-6, 5.158568e-4, -0.1126655495, 13.6175032, -57.96288],
-}
-
-const coefficient = (weight: number, gender: Gender) => {
-  const c = params[gender]
-  const w = weight
-  const numerator = 500
-
-  return (
-    numerator /
-    (c[0] * w ** 4 + c[1] * w ** 3 + c[2] * w ** 2 + c[3] * w + c[4])
-  )
 }
 
 export const dots: DOTS = (
@@ -29,7 +19,11 @@ export const dots: DOTS = (
     liftedWeight = kg2lbs(liftedWeight)
   }
 
-  return format(liftedWeight * coefficient(bodyWeight, gender))
+  const numerator = 500
+
+  return format(
+    liftedWeight * coefficient(bodyWeight, params[gender], numerator)
+  )
 }
 
 export default dots
